85d01bf64ad5d76e02ec6c3d557fbc9f78288a4f
VaaKii <kohelev04@gmail.com>
2023-04-09 19:38:50 +0300

    ADD: Malloc

--- kernel/include/memory/memory.h
+++ kernel/include/memory/memory.h
@@ -8,6 +8,16 @@
 #include <stdint.h>
 #include <stddef.h>
 
+typedef struct {
+    uint8_t status;
+    uint32_t size;
+} alloc_t;
+
+extern void mm_init();
+extern char* pmalloc(size_t size); /* page aligned alloc */
+extern char* malloc(size_t size);
+extern void free(void *mem);
+extern void pfree(void *mem);
 extern void* memcpy(void* dest, const void* src, size_t count );
 extern void* memset (void * ptr, int value, size_t num );
 extern void* memset16 (void *ptr, uint16_t value, size_t num);
