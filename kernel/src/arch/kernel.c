c7192ff0fd440a1c4d9198b1712ba34c7074971c
VaaKii <kohelev04@gmail.com>
2023-04-09 19:39:05 +0300

    FIX: Add malloc, pic and pit

--- kernel/src/arch/kernel.c
+++ kernel/src/arch/kernel.c
@@ -3,12 +3,20 @@
 #include <memory/gdt.h>
 #include <memory/paging.h>
 #include <memory/idt.h>
+#include <memory/pic.h>
+#include <memory/pit.h>
+#include <memory/memory.h>
+
+extern uint32_t kernel_end;
 
 void kernel_main(void){
     terminal_initialize();
+    mm_init(&kernel_end);
     init_gdt();
     idt_init();
     exception_init();
     paging_init();
+    pic_init();
+    pit_init();
     asm volatile("hlt");
 }
