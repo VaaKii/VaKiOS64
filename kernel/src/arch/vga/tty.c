5f943d35267e5472f5378f1b92806d482a944242
VaKi <kohele04@gmail.com>
2023-07-04 07:59:53 +0300

    FIX: Delete strlen and move to string

--- kernel/src/arch/vga/tty.c
+++ kernel/src/arch/vga/tty.c
@@ -11,13 +11,7 @@ static inline uint16_t vga_entry(unsigned char uc, uint8_t color)
     return (uint16_t) uc | (uint16_t) color << 8;
 }
 
-size_t strlen(const char* str)
-{
-    size_t len = 0;
-    while (str[len])
-        len++;
-    return len;
-}
+
 
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
